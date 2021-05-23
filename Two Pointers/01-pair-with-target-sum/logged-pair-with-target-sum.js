a9f7d85cdf55b3ff8b4a21657426cefae87e4537
constantcoder <garrett@constantcoder.io>
2021-05-23 14:29:12 -0500

    👌 IMPROVE: added Two Pointers group

---
+++ Two Pointers/01-pair-with-target-sum/logged-pair-with-target-sum.js
@@ -0,0 +1,24 @@
+function pair_with_target_sum(arr, targetSum) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const currentSum = arr[left] + arr[right];
+
+    if (currentSum === targetSum) {
+      return [left, right];
+    }
+
+    if (targetSum > currentSum) {
+      // we need a pair with a bigger sum
+      left += 1;
+    } else {
+      // we need a pair with a smaller sum
+      right -= 1;
+    }
+  }
+  return [-1, -1];
+}
+
+console.log(pair_with_target_sum([1, 2, 3, 4, 6], 6));
+console.log(pair_with_target_sum([2, 5, 9, 11], 11));
